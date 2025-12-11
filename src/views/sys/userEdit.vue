9ea08be7ff90aa4e91b18fff3af1f166137013f5
unicornaxe21 <150046525+unicornaxe21@users.noreply.github.com>
2025-12-11 01:07:15 -0800

    后台用户管理 - update ui and functionalities

--- src/views/sys/userEdit.vue
+++ src/views/sys/userEdit.vue
@@ -2,10 +2,25 @@
     <el-dialog :title="title" v-model="isshow" width="500px" top="5vh" >
         <el-form>
             <el-form-item label="账号">
-                <el-input  v-model="param.username" placeholder="请输入" clearable :disabled="title == '账号编辑'" />
+                <el-input  v-model="param.username" placeholder="请输入" clearable :disabled="title == '编辑角色'" />
             </el-form-item>
-            <el-form-item label="密码">
+            <el-form-item label="密码" v-if="title == '新增角色'">
                 <el-input  v-model="param.password" placeholder="请输入" type="password" clearable />
+            </el-form-item>
+            <el-form-item label="角色">
+                <el-select 
+                    v-model="param.role_id" 
+                    placeholder="请选择角色" 
+                    clearable 
+                    style="width: 100%;"
+                >
+                    <el-option 
+                        v-for="role in rolesList" 
+                        :key="role.id" 
+                        :label="role.role" 
+                        :value="role.id" 
+                    />
+                </el-select>
             </el-form-item>
             <el-form-item label="备注">
                 <el-input  v-model="param.remark" placeholder="请输入" clearable />
@@ -25,19 +40,41 @@
     const isshow = ref(false);
     const title = ref('')
     const param = ref({});
+    const rolesList = ref([])
     const emit = defineEmits(['query']); //emit('query'); 
     defineExpose({ Open });
 
 
     function Open(data){
         param.value = {};
+        loadRoles();
         if(data){
             title.value = '编辑角色'
+            param.value.uid = data.id;
             param.value.username = data.account;
             param.value.remark = data.remark;
+            // Don't include password when editing
+            delete param.value.password;
+            // Set role_id from data.role (object with id), data.role_id, data.role_ids, or data.roles
+            if(data.role && typeof data.role === 'object' && data.role.id){
+                // If role is an object with id property (from API response)
+                param.value.role_id = data.role.id;
+            } else if(data.role_id){
+                param.value.role_id = data.role_id;
+            } else if(data.role_ids && Array.isArray(data.role_ids) && data.role_ids.length > 0){
+                // If role_ids is an array, take the first one
+                param.value.role_id = data.role_ids[0];
+            } else if(data.roles && Array.isArray(data.roles) && data.roles.length > 0){
+                // If roles is an array, extract the first role ID
+                const firstRole = data.roles[0];
+                param.value.role_id = typeof firstRole === 'object' ? firstRole.id : firstRole;
+            } else {
+                param.value.role_id = '';
+            }
         }
         else{
             title.value = '新增角色'
+            param.value.role_id = '';
         }
         isshow.value = true;
     }
@@ -64,6 +101,14 @@
                 else tips.error(res.msg)
             }).finally(()=>{ loading.close(); });
         }
+    }
+    //加载角色列表
+    function loadRoles(){
+        axios.post('/admin/roles/list', {}).then( res => {
+            if (res.status == 200) {
+                rolesList.value = res.data.roles || [];
+            }
+        });
     }
 </script>
 
--- src/views/sys/userList.vue
+++ src/views/sys/userList.vue
@@ -1,38 +1,101 @@
 <template>
     <div class="page-t">
-        <el-scrollbar class="page-param" max-height="126px" style="margin-bottom: 5px;">
-            <el-form :inline="true">
-                <el-form-item label="账号">
-                    <el-input  v-model="param.account" placeholder="请输入" clearable />
-                </el-form-item>
-                <el-form-item label="IP">
-                    <el-input  v-model="param.ip" placeholder="请输入" clearable />
-                </el-form-item>
-            </el-form>
-        </el-scrollbar>
+        <div class="page-header">
+            <el-scrollbar class="page-param" max-height="126px" style="margin-bottom: 5px;">
+                <el-form :inline="true">
+                    <el-form-item label="后台用户">
+                        <el-input v-model="param.account" placeholder="请输入" clearable style="width: 180px;" />
+                    </el-form-item>
+                    <!-- <el-form-item label="登录名/昵称">
+                        <el-input v-model="param.keyword" placeholder="请输入登录名或昵称" clearable style="width: 180px;" />
+                    </el-form-item> -->
+                    <el-form-item label="角色">
+                        <el-select v-model="param.role_id" placeholder="全部" clearable style="width: 150px;">
+                            <el-option label="全部" value="" />
+                            <el-option 
+                                v-for="role in rolesList" 
+                                :key="role.id" 
+                                :label="role.role" 
+                                :value="role.id" 
+                            />
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item label="状态">
+                        <el-select v-model="param.status" placeholder="全部" clearable style="width: 120px;">
+                            <el-option label="全部" value="" />
+                            <el-option label="正常" value="1" />
+                            <el-option label="禁用" value="0" />
+                        </el-select>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button v-blur type="primary" icon="Search" @click="Query">查询</el-button>
+                        <el-button v-blur type="warning" icon="RefreshLeft" @click="funReset">重置</el-button>
+                        <el-button v-blur type="success" icon="Plus" @click.prevent="edit.Open(null)">新增</el-button>
+                    </el-form-item>
+                </el-form>
+            </el-scrollbar>
+        </div>
 
-        <el-scrollbar style="white-space: nowrap; margin-bottom: 10px;">
-            <el-button v-blur type="primary" icon="Search" @click="Query">  查询</el-button>
-            <el-button v-blur type="warning" icon="RefreshLeft" @click="funReset">  重置</el-button>
-            <el-button v-blur type="success" icon="Plus" @click.prevent="edit.Open(null)">添加</el-button>
-            <!-- <el-button v-blur type="danger" icon="Delete"  @click="funDeletes">删除</el-button>
-            <el-button v-blur type="info" icon="Download">导出</el-button> -->
-        </el-scrollbar>
-
-        <el-table :data="tableData.slice(0, 100)" @selection-change="SelectionChange" border max-height="100vh - 350px">
-            <el-table-column fixed type="selection" width="38" />
-            <el-table-column prop="" label="序号" width="70" >
-                <template #default="scope"> {{ scope.$index + 1 }} </template>
-            </el-table-column>
-            <el-table-column prop="id" label="UID" show-overflow-tooltip />
-            <el-table-column prop="account" label="账号" show-overflow-tooltip />
-            <el-table-column prop="datetime" label="注册时间" show-overflow-tooltip />
-            <el-table-column prop="logintime" label="登入时间" show-overflow-tooltip />
-            <el-table-column prop="remark" label="备注" show-overflow-tooltip />
-            <el-table-column fixed="right" label="操作" width="150">
+        <el-table :data="tableData" @selection-change="SelectionChange" border max-height="calc(100vh - 350px)" v-loading="loading">
+            <el-table-column fixed type="selection" width="55" align="center" />
+            <el-table-column prop="id" label="用户ID" width="90" align="center" show-overflow-tooltip />
+            <el-table-column prop="account" label="用户名" min-width="120" show-overflow-tooltip />
+            <el-table-column prop="nickname" label="昵称" min-width="120" show-overflow-tooltip>
                 <template #default="scope">
-                    <el-button link type="primary" icon="Edit" @click.prevent="edit.Open(scope.row)">  编辑</el-button>
-                    <el-button link type="danger" icon="Delete"  @click.prevent="Delete(scope.row.id)">删除</el-button>
+                    {{ scope.row.nickname || scope.row.account || '--' }}
+                </template>
+            </el-table-column>
+            <el-table-column prop="name" label="姓名" min-width="100" show-overflow-tooltip>
+                <template #default="scope">
+                    {{ scope.row.name || '--' }}
+                </template>
+            </el-table-column>
+            <el-table-column prop="phone" label="电话" min-width="130" show-overflow-tooltip>
+                <template #default="scope">
+                    {{ scope.row.phone || '--' }}
+                </template>
+            </el-table-column>
+            <el-table-column prop="department" label="部门" min-width="100" show-overflow-tooltip>
+                <template #default="scope">
+                    {{ scope.row.department || '--' }}
+                </template>
+            </el-table-column>
+            <el-table-column prop="position" label="职位" min-width="100" show-overflow-tooltip>
+                <template #default="scope">
+                    {{ scope.row.position || '--' }}
+                </template>
+            </el-table-column>
+            <el-table-column prop="roles" label="所属角色" min-width="180" show-overflow-tooltip>
+                <template #default="scope">
+                    <div v-if="getRolesArray(scope.row).length > 0" class="role-tags">
+                        <el-tag 
+                            v-for="(role, index) in getRolesArray(scope.row)" 
+                            :key="index" 
+                            type="primary" 
+                            size="small"
+                            class="role-tag"
+                        >
+                            {{ role }}
+                        </el-tag>
+                    </div>
+                    <span v-else>--</span>
+                </template>
+            </el-table-column>
+            <el-table-column prop="status" label="状态" width="100" align="center">
+                <template #default="scope">
+                    <el-tag v-if="scope.row.status == 1 || scope.row.status == undefined" type="success" size="small">正常</el-tag>
+                    <el-tag v-else type="danger" size="small">禁用</el-tag>
+                </template>
+            </el-table-column>
+            <el-table-column fixed="right" label="操作" width="220" align="center">
+                <template #default="scope">
+                    <el-button link type="primary" icon="Edit" @click.prevent="edit.Open(scope.row)">修改</el-button>
+                    <el-popconfirm title="确定删除?" @confirm="Delete(scope.row.id)">
+                        <template #reference>
+                            <el-button link type="danger" icon="Delete" @click.prevent>删除</el-button>
+                        </template>
+                    </el-popconfirm>
+                    <el-button link type="warning" icon="Key" @click.prevent="openResetPassword(scope.row)">重置密码</el-button>
                 </template>
             </el-table-column>
         </el-table>
@@ -52,6 +115,39 @@
         </el-scrollbar>
         
         <Edit1 ref="edit" @query="Query"></Edit1>
+        
+        <!-- 重置密码模态框 -->
+        <el-dialog v-model="resetPasswordDialog" title="重置密码" width="500px" top="5vh">
+            <el-form :model="resetPasswordForm" label-width="100px">
+                <el-form-item label="用户名">
+                    <el-input v-model="resetPasswordForm.account" disabled />
+                </el-form-item>
+                <el-form-item label="新密码" required>
+                    <el-input 
+                        v-model="resetPasswordForm.password" 
+                        type="password" 
+                        placeholder="请输入新密码" 
+                        show-password
+                        clearable 
+                    />
+                </el-form-item>
+                <el-form-item label="确认密码" required>
+                    <el-input 
+                        v-model="resetPasswordForm.confirmPassword" 
+                        type="password" 
+                        placeholder="请再次输入新密码" 
+                        show-password
+                        clearable 
+                    />
+                </el-form-item>
+            </el-form>
+            <template #footer>
+                <div class="dialog-footer">
+                    <el-button @click="resetPasswordDialog = false">取消</el-button>
+                    <el-button type="primary" @click="submitResetPassword">确定</el-button>
+                </div>
+            </template>
+        </el-dialog>
     </div>
 </template>
 
@@ -62,19 +158,31 @@
     const param = ref({page:1, limit:20});
     const total = ref(0);
     const tableData = ref([])
+    const rolesList = ref([])
+    const loading = ref(false)
+    const resetPasswordDialog = ref(false)
+    const resetPasswordForm = ref({
+        id: '',
+        account: '',
+        password: '',
+        confirmPassword: ''
+    })
 
-    setTimeout(()=>{ Query() }, 1)
+    setTimeout(()=>{ 
+        Query()
+        loadRoles()
+    }, 1)
     
     //查询
     function Query(){
-        let loading = tips.loading('查询中');
+        loading.value = true;
         axios.post('/admin/admin/list', param.value).then( res => {
             if (res.status == 200) {
-                tableData.value = res.data.list;
-                total.value = res.data.count;
+                tableData.value = res.data.list || [];
+                total.value = res.data.count || 0;
             }
             else tips.error(res.msg)
-        }).finally(()=>{ loading.close(); });
+        }).finally(()=>{ loading.value = false; });
     }
     //重置
     function funReset(){
@@ -120,13 +228,156 @@
         param.value.limit = number;
         Query()
     }
+    //加载角色列表
+    function loadRoles(){
+        axios.post('/admin/roles/list', {}).then( res => {
+            if (res.status == 200) {
+                rolesList.value = res.data.roles || [];
+            }
+        });
+    }
+    //打开重置密码对话框
+    function openResetPassword(row){
+        resetPasswordForm.value = {
+            id: row.id,
+            account: row.account || row.username || '',
+            password: '',
+            confirmPassword: ''
+        };
+        resetPasswordDialog.value = true;
+    }
+    //获取角色数组（处理字符串、数组等不同格式）
+    function getRolesArray(row){
+        // 如果 roles 是数组，直接返回
+        if(Array.isArray(row.roles)){
+            return row.roles;
+        }
+        // 如果 role 是对象，提取 role.role 属性
+        if(row.role && typeof row.role === 'object' && !Array.isArray(row.role)){
+            return row.role.role ? [row.role.role] : [];
+        }
+        // 如果 role 是数组，直接返回
+        if(Array.isArray(row.role)){
+            return row.role;
+        }
+        // 如果 roles 是字符串，尝试解析
+        if(typeof row.roles === 'string'){
+            try {
+                const parsed = JSON.parse(row.roles);
+                if(Array.isArray(parsed)){
+                    return parsed;
+                }
+            } catch(e) {
+                // 如果不是JSON，当作单个角色处理
+                return row.roles ? [row.roles] : [];
+            }
+        }
+        // 如果 role 是字符串，返回数组
+        if(typeof row.role === 'string' && row.role){
+            return [row.role];
+        }
+        // 如果 role_name 存在
+        if(row.role_name){
+            return [row.role_name];
+        }
+        return [];
+    }
+    //提交重置密码
+    function submitResetPassword(){
+        if(!resetPasswordForm.value.password){
+            tips.error('请输入新密码');
+            return;
+        }
+        if(resetPasswordForm.value.password !== resetPasswordForm.value.confirmPassword){
+            tips.error('两次输入的密码不一致');
+            return;
+        }
+        if(resetPasswordForm.value.password.length < 6){
+            tips.error('密码长度不能少于6位');
+            return;
+        }
+        
+        let loading = tips.loading('重置中');
+        axios.post('/admin/admin/pwd', {
+            uid: resetPasswordForm.value.id,
+            password: resetPasswordForm.value.password
+        }).then( res => {
+            if (res.status == 200) {
+                tips.success('密码重置成功');
+                resetPasswordDialog.value = false;
+                resetPasswordForm.value = {
+                    id: '',
+                    account: '',
+                    password: '',
+                    confirmPassword: ''
+                };
+            }
+            else tips.error(res.msg)
+        }).finally(()=>{ loading.close(); });
+    }
 
 </script>
 
 <style scoped>
-    .avatar{
-        width: 40px;
-        height: 40px;
-        border-radius: 999px;
-    }
+.page-t {
+    padding: 20px;
+    background-color: #fff;
+}
+
+.page-header {
+    margin-bottom: 15px;
+    padding: 15px;
+    background-color: #fafafa;
+    border-radius: 4px;
+}
+
+.page-param {
+    background-color: #fff;
+    padding: 10px;
+    border-radius: 4px;
+}
+
+.role-tags {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 6px;
+}
+
+.role-tag {
+    margin: 0;
+}
+
+:deep(.el-table) {
+    font-size: 14px;
+}
+
+:deep(.el-table th) {
+    background-color: #f5f7fa;
+    font-weight: 600;
+    color: #606266;
+}
+
+:deep(.el-table td) {
+    padding: 12px 0;
+}
+
+:deep(.el-button--link) {
+    padding: 0 8px;
+    font-size: 13px;
+}
+
+:deep(.el-tag) {
+    border-radius: 4px;
+    font-weight: 500;
+}
+
+:deep(.el-form-item) {
+    margin-bottom: 15px;
+    margin-right: 20px;
+}
+
+:deep(.el-form-item__label) {
+    font-weight: 500;
+    color: #606266;
+}
 </style>
