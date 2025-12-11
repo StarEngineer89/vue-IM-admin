e86ce4b6b785b2cb8f7b836f91e4659a8cfedd04
unicornaxe21 <150046525+unicornaxe21@users.noreply.github.com>
2025-12-11 01:04:39 -0800

    Update reset password form

--- src/components/index/password.vue
+++ src/components/index/password.vue
@@ -1,15 +1,15 @@
 <template>
     <el-dialog title="修改密码" v-model="isshow" width="500px" top="5vh" >
-        <el-form>
+        <el-form label-width="100px">
             <!-- <el-form-item label="旧密码">
                 <el-input  v-model="param.password" placeholder="请输入" clearable :disabled="title == '账号编辑'" />
             </el-form-item> -->
             <el-form-item label="新密码">
                 <el-input  v-model="param.password" placeholder="请输入" type="password" clearable />
             </el-form-item>
-            <!-- <el-form-item label="密码确认">
-                <el-input  v-model="param.password" placeholder="请输入" type="password" clearable />
-            </el-form-item> -->
+            <el-form-item label="密码确认">
+                <el-input  v-model="param.confirmPassword" placeholder="请输入" type="password" clearable />
+            </el-form-item>
         </el-form>
         <template #footer>
             <div class="dialog-footer">
@@ -33,9 +33,21 @@
         isshow.value = true;
     }
     function Submit(){
+        if(!param.value.password){
+            tips.error('请输入新密码');
+            return;
+        }
+        if(param.value.password !== param.value.confirmPassword){
+            tips.error('两次输入的密码不一致');
+            return;
+        }
+        if(param.value.password.length < 6){
+            tips.error('密码长度不能少于6位');
+            return;
+        }
         let loading = tips.loading('操作中');
         param.value.uid = userInfo.value.uid
-        axios.post('/admin/admin/pass', param.value).then( res => {
+        axios.post('/admin/admin/pwd', param.value).then( res => {
             if (res.status == 200) {
                 tips.success('操作成功')
                 isshow.value = false
